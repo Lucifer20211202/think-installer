cf5cbac5243791508892403257074bd4b5d04ea4
lucifer <lucifer@qq.com>
2023-12-12 21:07:23 +0800

    feat: update

--- src/ThinkFramework.php
+++ src/ThinkFramework.php
@@ -24,9 +24,9 @@ class ThinkFramework extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        if ('topthink/framework' !== $package->getPrettyName()) {
-            throw new InvalidArgumentException('Unable to install this library!');
-        }
+//        if ('topthink/framework' !== $package->getPrettyName()) {
+//            throw new InvalidArgumentException('Unable to install this library!');
+//        }
 
         if ($this->composer->getPackage()->getType() !== 'project') {
             return parent::getInstallPath($package);
